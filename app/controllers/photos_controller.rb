ab45c04db36b0d84ff34dfe3010f9cf0e6e43e42
sdzubiria <34401013+sdzubiria@users.noreply.github.com>
2024-05-21 21:03:28 +0000

    create_photo_post problem

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,5 +6,51 @@ class PhotosController < ApplicationController
     
     render({ :template => "photo_templates/index" })
   end
-end
 
+  def show
+    
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+    
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show" })
+    
+  end
+
+  def baii
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+    
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/baii" })
+
+    redirect_to("/photos")
+
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    if a_new_photo.save
+      redirect_to("/photos/" + a_new_photo.id.to_s)
+    else
+      # Log the errors to see why it failed
+      logger.debug a_new_photo.errors.full_messages
+      flash[:error] = a_new_photo.errors.full_messages.to_sentence
+      redirect_to("/photos")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii" })
 
-  get("/users/:path_username", { :controller => "users",
-   :action => "show" })
-
-   get("/photos", { :controller => "photos", :action => "index" })
+  post("/insert_photo", { :controller => "photos", :action => "create" })
 end
